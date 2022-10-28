aa1747007ec7287cdc831d74d223e982d32f0504
jeongjuz <dnwn6704@gmail.com>
2022-10-28 15:33:00 +0900

    testdb

---
+++ db/testdb_stinfo-stgrade1.sql
@@ -0,0 +1,27 @@
+show databases;
+use testdb;
+show tables;
+
+create table st_info(ST_ID int, NAME varchar(20), DEPT varchar(25)) default charset=utf8;
+
+show tables;
+
+create table st_grade(ST_ID int, Linux int, DB int);
+
+show tables;
+
+explain st_info;
+explain st_grade;
+
+alter table st_info add constraint pk_stinfo primary key(ST_ID);
+alter table st_gradegff add constraint pk_stinfo primary key(ST_ID);
+
+insert into st_info values (202201,'LeeGillDong','Game');
+insert into st_info values (202202,'KimGillDong','Computer');
+insert into st_info values (202203,'HongGillDong','Game');
+select * from st_info;
+
+insert into st_grade values(202201,'80','70');
+insert into st_grade values(202202,'90','75');
+insert into st_grade values(202203,'95','85');
+select * from st_grade;
